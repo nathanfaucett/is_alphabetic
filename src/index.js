dcbbc47581b942915891d47d369794fbea913ff9
nathanfaucett <nathanfaucett@gmail.com>
2015-09-24 09:35:14 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ module.exports = isAlphabetic;
 
 
 function isAlphabetic(ch) {
-    var charCode = ch.charCodeAt(0);
+    return isAlphabeticCode(ch.charCodeAt(0));
+}
 
+isAlphabetic.code = isAlphabeticCode;
+
+function isAlphabeticCode(charCode) {
     return (
         (charCode >= 0x41 && charCode <= 0x5A) ||
         (charCode >= 0x61 && charCode <= 0x7A)
